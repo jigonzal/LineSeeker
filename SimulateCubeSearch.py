56ae049b63f05eced40fb26ebfc9127df045697d
jigonzal <jgonzal@astro.puc.cl>
2021-03-24 02:16:37 -0300

    Update SimulateCubeSearch.py

--- SimulateCubeSearch.py
+++ SimulateCubeSearch.py
@@ -1,5 +1,6 @@
 # import warnings
 # warnings.filterwarnings("ignore")
+from __future__ import print_function
 import numpy as np
 import astropy.io.fits as fits
 from astropy.table import Table
@@ -88,61 +89,61 @@ def main():
     args = parser.parse_args()
 
     #Checking input arguments
-    print 20*'#','Checking inputs....',20*'#'
+    print(20*'#','Checking inputs....',20*'#')
     if os.path.exists(args.Cube):
-        print '*** Cube',args.Cube,'found ***'
+        print('*** Cube',args.Cube,'found ***')
     else:
-        print '*** Cube',args.Cube,'not found ***\naborting..'
+        print('*** Cube',args.Cube,'not found ***\naborting..')
         exit()
 
 
     if os.path.exists(args.OutputPath):
-        print '*** Directory',args.OutputPath,'exists ***\naborting..'
+        print('*** Directory',args.OutputPath,'exists ***\naborting..')
         exit()
     else:
-        print '*** Creating Directory',args.OutputPath,' ***'
+        print('*** Creating Directory',args.OutputPath,' ***')
         os.mkdir(args.OutputPath)
 
  
     if args.MaxSigmas<1:
-        print '*** The value for MaxSigmas of',args.MaxSigmas,'is too small ***\naborting..'
+        print('*** The value for MaxSigmas of',args.MaxSigmas,'is too small ***\naborting..')
         exit()
     else:
-        print '*** The value for MaxSigmas of',args.MaxSigmas,'is ok ***'
+        print('*** The value for MaxSigmas of',args.MaxSigmas,'is ok ***')
 
 
     if args.MinSN<0:
-        print '*** The value for MinSN of',args.MinSN,'has to be positive ***\naborting..'
+        print('*** The value for MinSN of',args.MinSN,'has to be positive ***\naborting..')
         exit()
 #     elif args.MinSN<3.5:
 #         print '*** The value for MinSN of',args.MinSN,'is smaller than needed but usable, expect larger files ***'
     else:
-        print '*** The value for MinSN of',args.MinSN,'is ok ***'
+        print('*** The value for MinSN of',args.MinSN,'is ok ***')
 
     UseMask = False
     if args.UseMask=='True':
-        print '*** Will use Continuum image to create mask ***'
+        print('*** Will use Continuum image to create mask ***')
         UseMask = True
     else:
-        print '*** Will not use Continuum image to create mask ***'
+        print('*** Will not use Continuum image to create mask ***')
 
     if UseMask:
             if os.path.exists(args.ContinuumImage):
-                print '*** Continuum Image',args.ContinuumImage,'found ***'
+                print('*** Continuum Image',args.ContinuumImage,'found ***')
             else:
-                print '*** Continuum Image',args.ContinuumImage,'not found ***\naborting..'
+                print('*** Continuum Image',args.ContinuumImage,'not found ***\naborting..')
                 exit()
 
                 if args.MaskSN<0:
-                    print '*** The value for MaskSN of',args.MinSN,'has to be positive ***\naborting..'
+                    print('*** The value for MaskSN of',args.MinSN,'has to be positive ***\naborting..')
                     exit()
                 else:
-                    print '*** The value for MaskSN of',args.MinSN,' is ok ***'
+                    print('*** The value for MaskSN of',args.MinSN,' is ok ***')
 
     if args.Kernel=='Gaussian' or args.Kernel=='gaussian':
-        print '*** Using Gaussian Kernel ***'
+        print('*** Using Gaussian Kernel ***')
     else:
-        print '*** Using Tophat Kernel ***'
+        print('*** Using Tophat Kernel ***')
 
     #finding channel width
     Header = fits.open(args.Cube)[0].header
@@ -151,14 +152,14 @@ def main():
     ApproxChannelVelocityWidth = (abs(ChannelSpacing)/RefFrequency)*3e5
     ApproxMaxSigmas = int((1000.0/ApproxChannelVelocityWidth)/2.35) + 1
     if args.Kernel=='Gaussian' or args.Kernel=='gaussian':
-        print '*** MaxSigmas should be of the order of ',ApproxMaxSigmas,'to detect a line width FWHM ~ 1000 km/s ***'
+        print('*** MaxSigmas should be of the order of ',ApproxMaxSigmas,'to detect a line width FWHM ~ 1000 km/s ***')
     else:
         ApproxMaxSigmas = int ((1000.0/ApproxChannelVelocityWidth))+1
-        print '*** MaxSigmas should be of the order of ',ApproxMaxSigmas,'to detect a line width of ~ 1000 km/s for the Tophat Kernel (considering the reference frequency CRVAL3)***'
+        print('*** MaxSigmas should be of the order of ',ApproxMaxSigmas,'to detect a line width of ~ 1000 km/s for the Tophat Kernel (considering the reference frequency CRVAL3)***')
     
 
     for SimulationIndex in range(args.NSimulations):
-        print '*** Cube: '+str(SimulationIndex+1)+'/'+str(args.NSimulations)+' ***'
+        print('*** Cube: '+str(SimulationIndex+1)+'/'+str(args.NSimulations)+' ***')
         LineSeekerFunctions.SimulateCube(args.Cube)
         os.mkdir(args.OutputPath+'/simul_'+str(SimulationIndex))
         NewPathOutput = args.OutputPath+'/simul_'+str(SimulationIndex)
